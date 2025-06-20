115e60b1dd42f925ba3299ab4e70052c1b8d4356
phR0ze <prc451@gmail.com>
2025-06-20 10:51:10 -0600

    Re-indenting to 2 spaces for main.rs

--- api/src/main.rs
+++ api/src/main.rs
@@ -11,14 +11,14 @@ const APP_NAME: &str = "OneUp";
 // Keeping non-async for configuration and observability
 fn main() -> anyhow::Result<()> 
 {
-    // Init configuration and observability
-    let config = state::config::init()?;
-    utils::observe::init(APP_NAME, &config);
+  // Init configuration and observability
+  let config = state::config::init()?;
+  utils::observe::init(APP_NAME, &config);
 
-    // Start the api server
-    serve(config)?;
+  // Start the api server
+  serve(config)?;
 
-    Ok(())
+  Ok(())
 }
 
 /// Start the tokio runtime manually to allow for:
@@ -26,28 +26,28 @@ fn main() -> anyhow::Result<()>
 /// * Handling configuration and observability setup before starting the server
 fn serve(config: model::Config) -> anyhow::Result<()> 
 {
-    tokio::runtime::Builder::new_multi_thread()
-        //.worker_threads(config.runtime.worker_threads)
-        //.thread_name(APP_NAME)
-        //.thread_stack_size(3 * 1024 * 1024)
-        .enable_all()
-        .build()?
+  tokio::runtime::Builder::new_multi_thread()
+    //.worker_threads(config.runtime.worker_threads)
+    //.thread_name(APP_NAME)
+    //.thread_stack_size(3 * 1024 * 1024)
+    .enable_all()
+    .build()?
 
-        // Initialize Axum inside the tokio runtime
-        .block_on(async move 
-        {
-            let addr = format!("{}:{}", &config.ip, config.port);
-            let state = state::init(config).await?;
-            let router = routes::init(std::sync::Arc::new(state));
-            log::info!("Server started at: {}", addr);
+    // Initialize Axum inside the tokio runtime
+    .block_on(async move 
+    {
+      let addr = format!("{}:{}", &config.ip, config.port);
+      let state = state::init(config).await?;
+      let router = routes::init(std::sync::Arc::new(state));
+      log::info!("Server started at: {}", addr);
 
-            let listener = tokio::net::TcpListener::bind(addr).await?;
-            axum::serve(listener, router.into_make_service()).await?;
+      let listener = tokio::net::TcpListener::bind(addr).await?;
+      axum::serve(listener, router.into_make_service()).await?;
 
-            Ok::<(), anyhow::Error>(())
-        })?;
+      Ok::<(), anyhow::Error>(())
+    })?;
 
-    Ok(())
+  Ok(())
 }
 
 // use std::env;
@@ -83,4 +83,4 @@ fn serve(config: model::Config) -> anyhow::Result<()>
 //   println!("Commands:");
 //   println!("  token   Generate a new api token");
 //   println!("  run     Run the api server");
-// }
+// } 
